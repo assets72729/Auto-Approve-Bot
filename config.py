52cde274811742578f7f101e7af8d6f9d405b793
assets72729 <hunxutia@gmail.com>
2025-07-17 21:46:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from typing import List
 API_ID = os.environ.get("API_ID", "24890303")
 API_HASH = os.environ.get("API_HASH", "94cf78d1e6883ecb10f32e31fc23cfe0")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", "7357122047"))
+ADMIN = int(os.environ.get("ADMIN", "7289855833"))
 PICS = (os.environ.get("PICS", "https://graph.org/file/36d6bd0273c1f68bef35a-a4ea33ce86c9732e94.jpg")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002703468153"))
