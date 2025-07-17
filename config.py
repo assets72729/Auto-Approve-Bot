ae80a1b457cb2dfe023e3e2329c40ed8cec4c6db
assets72729 <hunxutia@gmail.com>
2025-07-17 21:12:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = os.environ.get("API_ID", "24890303")
 API_HASH = os.environ.get("API_HASH", "94cf78d1e6883ecb10f32e31fc23cfe0")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", "7357122047"))
-PICS = (os.environ.get("PICS", "")).split()
+PICS = (os.environ.get("PICS", "https://graph.org/file/36d6bd0273c1f68bef35a-a4ea33ce86c9732e94.jpg")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002895471764"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "True").lower() == "true"  # Set "True" For accept new requests
