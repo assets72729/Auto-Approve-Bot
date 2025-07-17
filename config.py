cc46c3cf1e10f5e4e7b73d15d7852eadc9156284
assets72729 <hunxutia@gmail.com>
2025-07-17 21:01:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 IS_FSUB = os.environ.get("IS_FSUB", "True").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002157930558").split())) # Add Multiple channel ids
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002705791364").split())) # Add Multiple channel ids
